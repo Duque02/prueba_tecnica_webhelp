3cfc195bf1bc48261aa8aa04dfc561bbdca4a73b
Miguel Duque <miguelangelduquecuervo@gmail.com>
2024-01-13 23:16:36 -0500

    add -Database connection

--- Webhelp.PruebaTecnica.API/Program.cs
+++ Webhelp.PruebaTecnica.API/Program.cs
@@ -1,3 +1,7 @@
+using Webhelp.PruebaTecnica.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +10,16 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Setup database
+
+var dbConnectionString = builder.Configuration.GetConnectionString("MedicalCenterDB");
+var serverVersion = ServerVersion.AutoDetect(dbConnectionString);
+
+builder.Services.AddDbContext<MedicalCenterDBContext>(
+    options => options.UseMySql(dbConnectionString, serverVersion)
+);
+
 
 var app = builder.Build();
 
--- Webhelp.PruebaTecnica.Infrastructure/Entities/PatientEntity.cs
+++ Webhelp.PruebaTecnica.Infrastructure/Entities/PatientEntity.cs
@@ -5,20 +5,25 @@ using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace Webhelp.PruebaTecnica.Infrastructure.Entities
 {
     [Table("Patients")]
+    [Index(nameof(Document), IsUnique = true)]
     internal class PatientEntity
     {
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         int PatientId { get; set; }
+
         string Name { get; set; }
+
         string LastName { get; set; }
 
         [ForeignKey("DocumentType")]
         int DocumentTypeId { get; set; }
+
         string Document { get; set; }
 
        DocumentTypeEntity DocumentType { get; set; }
